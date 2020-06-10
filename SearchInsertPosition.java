72359dda402cdfbe791bde81361eebb0abecf02d
Vipul Gupta <guptavipul09@gmail.com>
2020-06-10 23:26:53 +0530

    Add files via upload

---
+++ SearchInsertPosition.java
@@ -0,0 +1,26 @@
+package com.leetcode.June;
+
+public class SearchInsertPosition {
+	public static void main(String[] args) {
+		int[] arr= {1,3,5,6};
+		System.out.println(binarySearch(arr, 7));
+	}
+	public static int binarySearch(int []nums,int target) {
+		int start=0;
+		int end=nums.length-1;
+		int mid=0;
+		while(start<=end) {
+			 mid=start+(end-start)/2;
+			if(nums[mid]==target)
+				return mid;
+			if(nums[mid]<target) {
+				start=mid+1;
+			}else {
+				end=mid-1;
+			}
+		}
+		
+		return start;
+		
+	}
+}
